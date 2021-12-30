12f6d3480cd1c5b469bd787f1c313128089f72c4
Bouazza <Bouazza@DESKTOP-BE1S3RB>
2021-12-30 19:07:35 +0100

    Added the option to save and load an existing list

--- AppVerbe.ViewModels/AppSession.cs
+++ AppVerbe.ViewModels/AppSession.cs
@@ -11,9 +11,7 @@ namespace AppVerbe.ViewModels
         public AppData AppData { get; set; }
         public AppSession()
         {
-            AppData = SavingService.LoadListOrCreateNewOne("TestList.omar");
 
-            SavingService.SaveList(AppData, "OmarList.omar");
         }
     }
 }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,16 +1,56 @@
 ﻿using System.Windows;
+using System;
+using Microsoft.Win32;
 using AppVerbe.ViewModels;
+using AppVerbe.Services;
 
 namespace WPFUI
 {
     public partial class MainWindow : Window
     {
-        public AppSession _appSession;
+        private const string SAVE_LIST_FILE_EXTENSION = "omar";
+        private AppSession _appSession;
         public MainWindow()
         {
             InitializeComponent();
 
             _appSession = new AppSession();
+        }
+        private void SaveGame_OnClick(object sender, RoutedEventArgs e)
+        {
+            SaveGame();
+        }
+        private void SaveGame()
+        {
+            SaveFileDialog saveFileDialog =
+                new SaveFileDialog
+                {
+                    InitialDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                    Filter = $"Save games (*.{SAVE_LIST_FILE_EXTENSION})|*.{SAVE_LIST_FILE_EXTENSION}"
+                };
+
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                SavingService.SaveList(_appSession.AppData, saveFileDialog.FileName);
+            }
+        }
+        private void LoadGame_OnClick(object sender, RoutedEventArgs e)
+        {
+            LoadGame();
+        }
+        private void LoadGame()
+        {
+            OpenFileDialog openFileDialog =
+                new OpenFileDialog
+                {
+                    InitialDirectory = AppDomain.CurrentDomain.BaseDirectory,
+                    Filter = $"Save games (*.{SAVE_LIST_FILE_EXTENSION})|*.{SAVE_LIST_FILE_EXTENSION}"
+                };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _appSession.AppData = SavingService.LoadListOrCreateNewOne(openFileDialog.FileName);
+            }
         }
     }
 }
