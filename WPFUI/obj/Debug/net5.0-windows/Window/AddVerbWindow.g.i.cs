f8cc826a5703a58e52247780a34cd08fe7d0d23a
Bouazza <Bouazza@DESKTOP-BE1S3RB>
2022-03-25 19:38:27 +0100

    CustomConverter

--- WPFUI/obj/Debug/net5.0-windows/Window/AddVerbWindow.g.i.cs
+++ WPFUI/obj/Debug/net5.0-windows/Window/AddVerbWindow.g.i.cs
@@ -117,7 +117,7 @@ namespace WPFUI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFUI;component/window/addverbwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFUI;V1.0.0.0;component/window/addverbwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Window\AddVerbWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
