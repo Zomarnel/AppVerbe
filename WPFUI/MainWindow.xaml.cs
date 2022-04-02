691dc235765194017481925c6f25cdf0f9bd4edc
Bouazza <Bouazza@DESKTOP-BE1S3RB>
2022-04-02 16:26:21 +0200

    NEED TO REFACTOR

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -13,14 +13,21 @@ namespace WPFUI
         private const string SAVE_LIST_FILE_EXTENSION = "omar";
         private AppSession _appSession;
 
+        private PositionOnCanvas Position_TopRightTextBlock = new PositionOnCanvas();
+        private PositionOnCanvas Position_TopLeftTextBlock = new PositionOnCanvas();
+        private PositionOnCanvas Position_BottomRightTextBlock = new PositionOnCanvas();
+        private PositionOnCanvas Position_BottomLeftTextBlock = new PositionOnCanvas();
+
         public MainWindow()
         {
             InitializeComponent();
 
             _appSession = new AppSession();
-           
+
             DataContext = _appSession;
         }
+
+        #region Events
         private void SaveGame_OnClick(object sender, RoutedEventArgs e)
         {
             SaveGame();
@@ -54,11 +61,23 @@ namespace WPFUI
             }
         }
 
+        private void MainCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ResizeButtons();
+
+            ResizeTextBlocks();
+        }
+
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
             ResizeButtons();
+
+            ResizeTextBlocks();
         }
 
+        #endregion Events
+
+        #region Functions
         private void SaveGame()
         {
             SaveFileDialog saveFileDialog =
@@ -73,6 +92,7 @@ namespace WPFUI
                 SavingService.SaveList(_appSession.AppData, saveFileDialog.FileName);
             }
         }
+
         private void LoadGame()
         {
             OpenFileDialog openFileDialog =
@@ -87,6 +107,7 @@ namespace WPFUI
                 _appSession.AppData = SavingService.LoadListOrCreateNewOne(openFileDialog.FileName);
             }
         }
+
         private void ResizeButtons()
         {
             foreach (UIElement element in MainCanvas.Children)
@@ -116,9 +137,28 @@ namespace WPFUI
             }
         }
 
-        private void MainCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        private void ResizeTextBlocks()
         {
-            ResizeButtons();
+
+            double width = TopRightButton.ActualHeight;
+
+            //Top right buttons and textblocks
+            Canvas.SetRight(TopRightTextBlock, Canvas.GetRight(TopRightButton) + (TopRightButton.ActualWidth/2 - TopRightTextBlock.ActualWidth) / 2);
+            Canvas.SetBottom(TopRightTextBlock, Canvas.GetBottom(TopRightButton) + (TopRightButton.ActualHeight - TopRightTextBlock.ActualHeight) / 2);
+
+            //Top left buttons and textblocks
+            Canvas.SetRight(TopLeftTextBlock, Canvas.GetRight(MainButton) + (MainButton.ActualWidth / 2 - TopLeftTextBlock.ActualWidth) / 2);
+            Canvas.SetBottom(TopLeftTextBlock, Canvas.GetBottom(MainButton) + (MainButton.ActualHeight - TopLeftTextBlock.ActualHeight) / 2);
+
+            //Bottom right buttons and textblocks
+            Canvas.SetRight(BottomRightTextBlock, Canvas.GetRight(BottomRightButton) + (BottomRightButton.ActualWidth / 2 - BottomRightTextBlock.ActualWidth) / 2);
+            Canvas.SetBottom(BottomRightTextBlock, Canvas.GetBottom(BottomRightButton) + (BottomRightButton.ActualHeight - BottomRightTextBlock.ActualHeight) / 2);
+
+            //Bottom left buttons and textblocks
+            Canvas.SetRight(BottomLeftTextBlock, Canvas.GetRight(BottomLeftButton) + (BottomLeftButton.ActualWidth / 2 - BottomLeftTextBlock.ActualWidth) / 2);
+            Canvas.SetBottom(BottomLeftTextBlock, Canvas.GetBottom(BottomLeftButton) + (BottomLeftButton.ActualHeight/2 - BottomLeftTextBlock.ActualHeight) / 2);
+
         }
+        #endregion
     }
 }
