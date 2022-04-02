f45dcec13a055d2cc5cc89bc327639400d92e3c5
Bouazza <Bouazza@DESKTOP-BE1S3RB>
2022-04-02 12:04:33 +0200

    FInally

--- WPFUI/CustomConverters/HalfValueConverter.cs
+++ WPFUI/CustomConverters/HalfValueConverter.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
 
 namespace WPFUI.CustomConverters
 {
-    class HalfValueConverter
+    public class HalfValueConverter : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {   
+
+            double totalHeight = (double)value;
+            return (object)(totalHeight / 2);
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,11 +1,10 @@
 ﻿using System.Windows;
 using System;
+using System.Windows.Controls;
 using Microsoft.Win32;
-using System.Windows.Input;
 using AppVerbe.ViewModels;
 using AppVerbe.Services;
 using AppVerbe.Models;
-using System.Collections.Generic;
 
 namespace WPFUI
 {
@@ -19,12 +18,8 @@ namespace WPFUI
             InitializeComponent();
 
             _appSession = new AppSession();
-
            
             DataContext = _appSession;
-
-            
-
         }
         private void SaveGame_OnClick(object sender, RoutedEventArgs e)
         {
@@ -55,11 +50,15 @@ namespace WPFUI
 
                 randomVerbWindow.Owner = this;
                 randomVerbWindow.ShowDialog();
-
                 
             }
         }
-        
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            ResizeButtons();
+        }
+
         private void SaveGame()
         {
             SaveFileDialog saveFileDialog =
@@ -88,6 +87,38 @@ namespace WPFUI
                 _appSession.AppData = SavingService.LoadListOrCreateNewOne(openFileDialog.FileName);
             }
         }
+        private void ResizeButtons()
+        {
+            foreach (UIElement element in MainCanvas.Children)
+            {
+                if (element is not Button)
+                {
+                    continue;
+                }
 
+                switch (((Button)element).Name)
+                {
+                    case "TopRightButton":
+                        Canvas.SetRight(element, Canvas.GetRight(MainButton) - 100);
+                        Canvas.SetBottom(element, Canvas.GetBottom(MainButton));
+                        break;
+
+                    case "BottomRightButton":
+                        Canvas.SetRight(element, Canvas.GetRight(MainButton) - 100);
+                        Canvas.SetBottom(element, Canvas.GetBottom(MainButton) - 100);
+                        break;
+
+                    case "BottomLeftButton":
+                        Canvas.SetRight(element, Canvas.GetRight(MainButton));
+                        Canvas.SetBottom(element, Canvas.GetBottom(MainButton) - 100);
+                        break;
+                }
+            }
+        }
+
+        private void MainCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ResizeButtons();
+        }
     }
 }
